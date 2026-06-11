77d03f1fb3770c528bafc5590aeb37796ef12481
Joseph Kim <thejykim1@gmail.com>
2026-06-11 16:49:46 -0700

    Add: simplify-path - submission-0

---
+++ Data Structures & Algorithms/simplify-path/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stack = []
+
+        for directory in path.split('/'):
+            if not directory or directory == '.':
+                continue
+            
+            if directory == '..':
+                if stack:
+                    stack.pop()
+            else:
+                stack.append(directory)
+        
+        return '/' + '/'.join(stack)
